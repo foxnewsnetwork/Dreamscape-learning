a603546e3ee576ab610fd95f333c0a95b6fb970a
foxnewsnetwork <foxnewsnetwork@gmail.com>
2011-08-28 11:22:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '11014e2a01148efc2f5cb9231c52d4cbdd143fe06d816021d0840c9dd48f9ea60f494c89cf7db4d7757bd1d8ff3b7ce13b85decfb6009a46c0bfe806e87a0aff'
